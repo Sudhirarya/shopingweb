18b1ffaabf4886ba25ddd467af2b37cddad6fc44
Ashish00Singh <ashish7232singh@hmail.com>
2024-03-17 21:51:56 +0530

    aa

--- src/Comp/Home/Navbar.jsx
+++ src/Comp/Home/Navbar.jsx
@@ -9,8 +9,8 @@ const Navbar = () => {
     return (
         <nav className="navbar p-0 navbar-expand-lg  navbar-dark sticky-top">
             <div className="container-fluid" id='navbar'>
-                <Link id='logo' className="navbar-brand" to="#">
-                    <img className='logo' src={logo} alt="" />
+                <Link id='logo' className="navbar-brand" to="/">
+                    <img className='logo' src={logo} alt="Operal-Shoping" />
                 </Link>
                 <button className="navbar-toggler p-1 m-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon "></span>
@@ -18,29 +18,29 @@ const Navbar = () => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <NavLink id='link' className="nav-link  mx-1" aria-current="page" to="/">Home</NavLink>
+                            <NavLink id='link' className="nav-link px-2  p-1" aria-current="page" to="/">Home</NavLink>
                         </li>
                         <li className="nav-item">
-                            <NavLink id='link' className="nav-link mx-1" aria-current="page" to="/man">Man</NavLink>
+                            <NavLink id='link' className="nav-link px-2 p-1 " aria-current="page" to="/man">Man</NavLink>
                         </li>
                         <li className="nav-item">
-                            <NavLink id='link' className="nav-link mx-1" aria-current="page" to="/woman">Woman</NavLink>
+                            <NavLink id='link' className="nav-link px-2 p-1" aria-current="page" to="/woman">Woman</NavLink>
                         </li>
                         <li className="nav-item">
-                            <NavLink id='link' className="nav-link mx-1" aria-current="page" to="/kids">Kids</NavLink>
+                            <NavLink id='link' className="nav-link px-2 p-1" aria-current="page" to="/kids">Kids</NavLink>
                         </li>
                         <li className="nav-item">
-                            <NavLink id='link' className="nav-link mx-1" aria-current="page" to="/electronic">Electronic</NavLink>
-                        </li>                        
+                            <NavLink id='link' className="nav-link px-2 p-1" aria-current="page" to="/electronic">Electronic</NavLink>
+                        </li>
                     </ul>
                     <form className="d-flex gap-4 me-3">
                         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                        <NavLink type="button" to='/Cart' className=" position-relative pt-2">
-                            <i className="fa-solid fa-cart-shopping " style={{ color: 'rgb(245, 241, 234)', fontSize:'20px' }}></i>
+                        <Link type="button" to='/Cart' className=" position-relative pt-2">
+                            <i className="fa-solid fa-cart-shopping " style={{ color: 'rgb(245, 241, 234)', fontSize: '20px' }}></i>
                             <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill" >
                                 {totalItems}</span>
 
-                        </NavLink>
+                        </Link>
                     </form>
                 </div>
             </div>
